f00d29d42d9e82777e4ab158d34a7919d95d981e
Fatima Ezzahra <felemenoun@gmail.com>
2022-01-12 02:13:41 +0100

    Update Script

--- src/index.js
+++ src/index.js
@@ -1,4 +1,47 @@
 import _ from 'lodash';
 import './style.css';
+import more from './images/more.png';
 
- 
+ let list = [
+     {
+        description: 'Task 1',
+        completed: true,
+        index: 1
+     },
+     {
+        description: 'Task 4',
+        completed: true,
+        index: 4
+     },
+     {
+        description: 'Task 2',
+        completed: false,
+        index: 2
+     },
+     {
+        description: 'Task 3',
+        completed: true,
+        index: 3
+     }
+ ];
+
+  const listTask = document.querySelector(".list");
+
+ window.addEventListener('load', () => {
+     list.sort((a, b) => {
+         return a.index - b.index;
+     });
+
+     for (let i = 0; i < list.length; i+=1){
+         listTask.innerHTML += `
+         <li class="d-flex-row">
+             <div class="list_task d-flex-row">
+                <input type="checkbox" name="task" class="checkbox">
+                <span>${list[i].description}</span>
+             </div>
+             <img src=${more} alt="more icon">
+            </li>
+            <li class="break_line_list"><hr></li>
+         `
+     };
+ });
