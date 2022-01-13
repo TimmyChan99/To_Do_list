db4cb2e7a905435559350ae32c98f90bb7ef9f0d
Fatima Ezzahra <felemenoun@gmail.com>
2022-01-13 11:56:21 +0100

    Update script >> Add the remove and add functionalities

--- src/index.js
+++ src/index.js
@@ -1,44 +1,96 @@
 import './style.css';
 import more from './images/more.png';
+import add from './modules/add.js';
+import removeTask from './modules/remove.js';
 
-const list = [
-  {
-    description: 'Task 1',
-    completed: true,
-    index: 1,
-  },
-  {
-    description: 'Task 4',
-    completed: true,
-    index: 4,
-  },
-  {
-    description: 'Task 2',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Task 3',
-    completed: true,
-    index: 3,
-  },
-];
+const list = [];
 
+class Task {
+  constructor(value) {
+    this.description = value;
+    this.completed = false;
+    this.index = list.length + 1;
+  }
+}
+
+// Add to array and Store it in localestorage
+
+const form = document.querySelector('#list_input');
+const input = document.querySelector('#input');
+
+// Display //
+
+const addBtn = document.querySelector('.add_btn');
 const listTask = document.querySelector('.list');
 
-window.addEventListener('load', () => {
-  list.sort((a, b) => a.index - b.index);
+function SaveAndDisplay() {
+  const newTask = new Task(input.value);
+  add(list, newTask);
 
-  for (let i = 0; i < list.length; i += 1) {
-    listTask.innerHTML += `
-         <li class="d-flex-row">
-             <div class="list_task d-flex-row">
-                <input type="checkbox" name="task" class="checkbox">
-                <span>${list[i].description}</span>
-             </div>
-             <img src=${more} alt="more icon">
-            </li>
-            <li class="break_line_list"><hr></li>
-         `;
-  }
+  // localStorage.setItem('ToDoList', JSON.stringify(list));
+
+  // add <li>
+  const li = document.createElement('li');
+  li.classList.add('li', 'd-flex-row');
+  listTask.appendChild(li);
+
+  // add <div>
+  const div = document.createElement('div');
+  div.classList.add('list_task', 'd-flex-row');
+  li.appendChild(div);
+
+  // add <input>
+  const inputTag = document.createElement('input');
+  inputTag.type = 'checkbox';
+  inputTag.classList.add('checkbox');
+  div.appendChild(inputTag);
+
+  // add <span>
+  const span = document.createElement('span');
+  span.innerText = input.value;
+  div.appendChild(span);
+
+  // add <img>
+  const moreBtn = document.createElement('img');
+  moreBtn.classList.add('more_btn');
+  moreBtn.id = newTask.index;
+  moreBtn.src = more;
+  moreBtn.alt = 'more icon';
+  li.appendChild(moreBtn);
+
+  // Display //
+
+  input.value = '';
+
+  // remove
+
+  moreBtn.addEventListener('click', (e) => {
+    const idNumer = (parseInt(e.target.id, 10) - 1);
+    e.target.parentNode.remove();
+    removeTask(list, idNumer);
+
+    // Update Index
+
+    list.forEach((task, i) => {
+      task.index = i + 1;
+    });
+
+    // Update id
+
+    const moreBtns = document.querySelectorAll('.more_btn');
+    moreBtns.forEach((btn) => {
+      if (btn.id > e.target.id) {
+        btn.id -= 1;
+      }
+    });
+
+    localStorage.setItem('ToDoList', JSON.stringify(list));
+  });
+}
+
+addBtn.addEventListener('click', SaveAndDisplay);
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  SaveAndDisplay();
+  input.blur();
 });
---
+++ src/modules/remove.js
@@ -0,0 +1,4 @@
+export default function removeTask(arr, index) {
+  arr.splice(index, 1);
+  return arr;
+}
